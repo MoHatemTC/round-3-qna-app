fe9d0653ca7f6955e1b85bdeb307797797beef6c
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-08-31 03:19:53 +0300

    add user api service

--- qna-app-server/src/user/user.service.ts
+++ qna-app-server/src/user/user.service.ts
@@ -1,4 +1,74 @@
-import { Injectable } from "@nestjs/common";
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException
+} from "@nestjs/common";
+import { PrismaService } from "../prisma.service.js";
+import * as bcrypt from "bcryptjs";
+import { CreateUserDTO } from "./create-user-dto.js";
+import { LoginUserDTO } from "./login-user-dto.js";
+import { JwtService } from "@nestjs/jwt";
+import { MailService } from "../mail/mail.service.js";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    private prismaService: PrismaService,
+    private jwtService: JwtService,
+    private mailService: MailService
+  ) {}
+
+  private async encryptPassword(plainText: string, saltRound: number) {
+    return await bcrypt.hash(plainText, saltRound);
+  }
+
+  async register({ name, email, password, role }: CreateUserDTO) {
+    const user = await this.prismaService.user.findUnique({
+      where: { email }
+    });
+
+    if (user) throw new ConflictException("This email is already registered");
+
+    const hashedPassword = await this.encryptPassword(password, 10);
+
+    await this.prismaService.user.create({
+      data: {
+        name,
+        email,
+        password_hash: hashedPassword,
+        role
+      }
+    });
+
+    await this.mailService.verifyEmail(email);
+
+    return {
+      message: "User registered successfully, please verify your mail."
+    };
+  }
+
+  async login({ email, password }: LoginUserDTO) {
+    const user = await this.prismaService.user.findUnique({
+      where: { email }
+    });
+
+    if (!user) {
+      throw new UnauthorizedException("Invalid email or password");
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password_hash);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException("Invalid email or password");
+    }
+
+    const token = await this.jwtService.signAsync({
+      id: user.id,
+      role: user.role
+    });
+
+    return {
+      token
+    };
+  }
+}
