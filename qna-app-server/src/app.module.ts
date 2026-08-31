54de54944f0f24218b2c8923ee9623cefa013947
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-08-31 03:16:03 +0300

    add mail module

--- qna-app-server/src/app.module.ts
+++ qna-app-server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller.js";
 import { AppService } from "./app.service.js";
 import { UserModule } from "./user/user.module.js";
+import { MailModule } from "./mail/mail.module.js";
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, MailModule],
   controllers: [AppController],
   providers: [AppService]
 })
