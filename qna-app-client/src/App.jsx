d5031549dda4e4e4107f6af7fcd90dc3b00c0294
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-09-02 23:29:28 +0300

    add auth routes

--- qna-app-client/src/App.jsx
+++ qna-app-client/src/App.jsx
@@ -1,10 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import RegisterPage from './pages/RegisterPage'
+import LoginPage from './pages/LoginPage'
+import { Proxy, PublicRoute } from './utils/proxy';
+import VerifyAccountPage from './pages/VerifyAccountPage';
 
 function App() {
   return (
-    <div className='text-amber-300 text-3xl'>
-      Hello World
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<PublicRoute />}>
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/verify-account' element={<VerifyAccountPage />} />
+        </Route>
+        <Route element={<Proxy />}> {/* add admin-panel and student dashboard here */} </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
